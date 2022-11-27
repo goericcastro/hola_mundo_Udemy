80335c3633f2be046a73035b3707f53e602c0785
Eric Castro <eric.castro.ingenieria@gmail.com>
2022-11-27 00:15:41 -0300

    variables
    tipos de variables

---
+++ index.py
@@ -0,0 +1,16 @@
+#Acá va un comentario
+if 3 > 5:
+    print("5 es mayor a 3")
+
+#Acá va un comentario
+#if 5 > 3: #Acá va otro comentario
+ #   print("5 es mayor a 3")
+
+x = 5
+y = "chanchito feliz"
+
+print(x, y)
+
+email = "eric.castro.ingenieria@gmail.com"
+
+print(email)
