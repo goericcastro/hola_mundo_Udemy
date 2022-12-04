c21a2c6e6d34cd74e161c57555b96df0024f3518
Eric Castro <eric.castro.ingenieria@gmail.com>
2022-12-04 11:52:11 -0300

    Input Lista
    Ejercicio lista input

---
+++ ejercicios_input.py
@@ -0,0 +1,7 @@
+datos = input("ingrese datos: ")
+lista = ["hola", "mundo", "casa", "direccion", "São Paulo"]
+
+if lista.count(datos) > 0:
+    print("El dato existe en la lista: ", datos)
+else:
+    print("El dato no existe en la lista: ", datos)
