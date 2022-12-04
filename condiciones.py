6af22a2de37711246056bcbf3723b0389ef80ff4
Eric Castro <eric.castro.ingenieria@gmail.com>
2022-12-04 11:43:00 -0300

    and or
    mas condicionales

--- condiciones.py
+++ condiciones.py
@@ -3,4 +3,11 @@ if 2 > 5:
 elif 2 < 5:
     print("2 menor a 5 en elif")
     
-print("devueve true") if 5 > 2 else print("devuelve false")
+print("devueve true") if 5 > 2 else print("devuelve false") #Operador ternario
+
+if 2 < 5 and 2 > 2: # and
+    print("condicion and devuelve true")
+    
+if 2 < 5 or 3 < 2: # or
+    print("codicion or es valida")
+    
