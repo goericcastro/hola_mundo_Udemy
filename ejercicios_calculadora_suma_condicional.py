2835fb0dc0e43249bea6b4501df65804082bb0b2
Eric Castro <eric.castro.ingenieria@gmail.com>
2022-12-04 12:40:32 -0300

    Calculadora Condnicional
    Calculadora con condicionales

--- ejercicios_calculadora_suma_condicional.py
+++ ejercicios_calculadora_suma_condicional.py
@@ -3,6 +3,10 @@ try:
     primer_numero = int(primer_numero)
 except:
     primer_numero = "chanchito feliz"
+
+if primer_numero == "chanchito feliz":
+    print("Ingresaste primer datos erroneo")
+    exit()
     
 segundo_numero = input("Ingrese segundo numero: ")
 
@@ -10,6 +14,10 @@ try:
     segundo_numero = int(segundo_numero)
 except:
     segundo_numero = "chanchito feliz"
+
+if segundo_numero == "chanchito feliz":
+    print("Ingresaste segundo datos erroneo")
+    exit()
 
 if primer_numero == "chanchito feliz" or segundo_numero == "chanchito feliz":
     
