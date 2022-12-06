a89facbe25e64825eab3213d55bf1a167efb2e9f
Eric Castro <eric.castro.ingenieria@gmail.com>
2022-12-05 22:06:59 -0300

    Create range
    range exercises

---
+++ range.py
@@ -0,0 +1,13 @@
+for x in range(6):
+    print(x)
+    
+for y in range(1, 6): #Nosotros colocamos de donde iniciamos a contar
+    print(y)
+    
+for z in range(3, 10): #En este caso iniciamos en 3
+    print(z)
+    
+for a in range(3, 15, 5): #en este caso damos donde iniciamos y damos el rango que saltamos para contar
+    print(a)
+else:
+    print("Hemos Terminado!")
