c08df656ce577b68064be58dac7fbe1e2b28fc32
Eric Castro <eric.castro.ingenieria@gmail.com>
2022-12-05 21:54:48 -0300

    Update for_caracter
    continue

--- for_caracter.py
+++ for_caracter.py
@@ -3,9 +3,15 @@
 # for c in usuario:
 #     print(c)
 
-usuario = ["Chanchito Feliz", "Alberto", "Roberto", "Feliz", 123]
+# usuario = ["Chanchito Feliz", "Alberto", "Roberto", "Feliz", 123]
 
-for c in usuario:
-    if c == "Feliz":
-        break
+# for c in usuario:
+#     if c == "Feliz":
+#         break
+#     print(c)
+
+usuarios = ["Chanchito Feliz", "Alberto", "Roberto", "Feliz", 54]
+for c in usuarios:
+    if c == "Roberto":
+        continue
     print(c)
