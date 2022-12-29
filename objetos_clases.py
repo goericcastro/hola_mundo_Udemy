8723e3fa77a5106d0287813116c4d0022338d25b
Eric Castro <eric.castro.ingenieria@gmail.com>
2022-12-29 14:46:08 -0300

    newVersion_objetos_clases

--- objetos_clases.py
+++ objetos_clases.py
@@ -3,9 +3,19 @@ class Usuario:
         self.nombre = nombre
         self.apellido = apellido
         
-usuario = Usuario("Alberto", "Feliz")
-usuario2 = Usuario("Chanchito", "Feliz")
-
-print(usuario.nombre, usuario.apellido, usuario2.nombre, usuario2.apellido)
+    def saludo(self):
+        print('Hola, mi nombre es señor:', self.nombre, self.apellido)
         
-    
+    def saludo1(self):
+        print('Hola, mi nombre es señorita:', self.nombre, self.apellido)
+        
+
+usuario = Usuario('Eric', 'Castro')
+usuario2 = Usuario('bran', 'Ticona')
+usuario3 = Usuario('dhara', 'Castro')
+
+usuario.saludo()
+usuario2.saludo()
+usuario3.saludo1()
+        
+        
