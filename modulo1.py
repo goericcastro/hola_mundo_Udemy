2db2c13ed5e0bf4d1694c0d1b3b250b4f6776116
Eric Castro <eric.castro.ingenieria@gmail.com>
2023-01-02 11:10:24 -0300

    1er Modulo

---
+++ modulo1.py
@@ -0,0 +1,5 @@
+import modulos
+
+print(modulos.mascotas)
+    
+    
---
+++ modulos.py
@@ -0,0 +1,4 @@
+mascotas = ['chanchito feliz', 'Felipe', 'Andres camargo']
+    
+def saludo(nombre):
+    print('Hola!', nombre)
