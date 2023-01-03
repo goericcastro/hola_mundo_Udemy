4dd8eae1226c12096e41f18b08bdd050195e16d2
Eric Castro <eric.castro.ingenieria@gmail.com>
2023-01-03 18:56:39 -0300

    moduloscase

--- modulo1.py
+++ modulo1.py
@@ -1,5 +1,11 @@
 import modulos
+from camelcase import CamelCase
+
+c = CamelCase()
+s = 'esta oración necesita camelcase'
+
+print(c.hump(s))
 
 print(modulos.mascotas)
     
-    
+modulos.saludo('Nicolas')
--- modulos.py
+++ modulos.py
@@ -2,3 +2,7 @@ mascotas = ['chanchito feliz', 'Felipe', 'Andres camargo']
     
 def saludo(nombre):
     print('Hola!', nombre)
+
+print(mascotas)
+
+saludo('nicolas')
