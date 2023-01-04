a09ad5f9a5977ade509b271811eca94b47236773
Eric Castro <eric.castro.ingenieria@gmail.com>
2023-01-04 12:17:49 -0300

    Create funcion_mayor_edad.py
    first exercise

---
+++ funcion_mayor_edad.py
@@ -0,0 +1,7 @@
+edad = int(input())
+
+if edad >= 18:
+    print('Su edad es:', edad, 'años de edad, entonces es mayor de edad')
+
+else:
+    print('Su edad es:', edad, 'alos de edad, entonces es menor de edad')
