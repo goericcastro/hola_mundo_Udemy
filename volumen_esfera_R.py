b746503d68125705ae4e948adb00a3e46d855c4e
Eric Castro <eric.castro.ingenieria@gmail.com>
2023-01-04 12:54:46 -0300

    Update Volumen
    dos métodos

--- volumen_esfera_R.py
+++ volumen_esfera_R.py
@@ -5,3 +5,11 @@ radio = int(input())
 volumen_esfera = 4 / 3 * math.pi * radio ** 3
 
 print(volumen_esfera)
+
+#Cálculo con función
+def volumen_esfera2(r):
+
+    return 4 / 3 * math.pi * r ** 3
+
+resultado = volumen_esfera2(radio)
+print(resultado)
