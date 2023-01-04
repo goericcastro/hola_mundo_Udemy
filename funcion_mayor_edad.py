571d6b76ac887bcdbfe4a7532656cd034d7e69c7
Eric Castro <eric.castro.ingenieria@gmail.com>
2023-01-04 13:17:42 -0300

    Update mayor_edad.py
    añadir segundo método

--- funcion_mayor_edad.py
+++ funcion_mayor_edad.py
@@ -5,3 +5,21 @@ if edad >= 18:
 
 else:
     print('Su edad es:', edad, 'alos de edad, entonces es menor de edad')
+    
+#2do Método con función
+
+def esMayor(usuario):
+    return usuario.edad > 18
+
+class Usuario:
+    def __init__(self, edad):
+        self.edad = edad
+        
+usuario = Usuario(edad)
+
+resultado = esMayor(usuario)
+
+if resultado == True:
+    print('Segundo Método, su edad es:', edad, 'años de edad, entonces es mayor de edad')
+else:
+    print('Segundo Método, su edad es:', edad, 'alos de edad, entonces es menor de edad')
