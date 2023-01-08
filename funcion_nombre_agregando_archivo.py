adb011d01943b76567f63d5255e4d613973b1cc4
Eric Castro <eric.castro.ingenieria@gmail.com>
2023-01-08 11:38:02 -0300

    Update funcion

--- funcion_nombre_agregando_archivo.py
+++ funcion_nombre_agregando_archivo.py
@@ -3,7 +3,7 @@ iapellido = input('Inserta apellido:')
 
 def agregaNombreArchivo(nombre, apellido):
     c = open('listaNombre.txt', 'a')
-    c.write(nombre + ' ' + apellido)
+    c.write(nombre + ' ' + apellido + '/n')
     c.close()
 
 agregaNombreArchivo(inombre, iapellido)
